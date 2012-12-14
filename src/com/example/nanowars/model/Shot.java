bd78ffc5b95b3119f8f9900894df2dac80fbdee4
Michał Rus <m@michalrus.com>
2012-12-14 14:27:55 +0100

    Improve shooting to a nearby target (when the shot is not visible)
    Decreased speed of a shot by 2×.

--- src/com/example/nanowars/model/Shot.java
+++ src/com/example/nanowars/model/Shot.java
@@ -3,8 +3,8 @@ package com.example.nanowars.model;
 public class Shot {
 	public int load;
 	public Cell from, to;
-	public double x, y, radius;
-	public boolean finished = false;
+	public double x, y, radius, distanceLeft, moveOverlap;
+	public boolean finished = false, started = false;
 	public Cell.Type type;
 
 	protected static int RADIUS_K = 3;
@@ -12,48 +12,50 @@ public class Shot {
 	protected static double RADIUS_AT_K = 0.04166666666666666666666666666667;
 	protected static double RADIUS_AT_L = 0.07291666666666666666666666666667;
 
-	private static double MOVE_VELOCITY = 0.01041666666666666666666666666667 / 50; // fraction of display per [ms]
-	private static double MOVE_OVERLAP = 0.0;
+	private static double MOVE_VELOCITY = 0.01041666666666666666666666666667 / 100; // fraction of display per [ms]
+	private static double MOVE_OVERLAP = 0.6; // fraction of shot radius
 
 	public Shot(Cell from, Cell to, int load) {
 		this.from = from;
 		this.to = to;
 		this.load = load;
-		this.x = from.x;
-		this.y = from.y;
+		x = from.x;
+		y = from.y;
 		type = from.type;
 		
 		double a = (RADIUS_AT_L - RADIUS_AT_K) / (RADIUS_L - RADIUS_K);
 		double b = RADIUS_AT_K - RADIUS_K * a;
 		radius = a * load + b;
-		
-		moveBy(from.radius + radius - MOVE_OVERLAP);
+		moveOverlap = radius * (1 - MOVE_OVERLAP);
 	}
 	
-	private void moveBy (double dist) {
-		if (finished)
-			return;
-		
-		double d = Game.distance(x, y, to.x, to.y);
-		
-		if (d - to.radius - radius < 0 || d - to.radius - radius + MOVE_OVERLAP < dist) {
-			finished = true;
-			return;
-		}
-
-		x += dist * (to.x - x) / d;
-		y += dist * (to.y - y) / d;
+	private void computeDistanceLeft () {
+		distanceLeft = Game.distance(x, y, to.x, to.y);
+	}
+	
+	private void moveCloserBy (double distance) {
+		x += distance * (to.x - x) / distanceLeft;
+		y += distance * (to.y - y) / distanceLeft;
 	}
 
 	public void update(long dt) {
 		if (finished)
 			return;
 
-		moveBy(MOVE_VELOCITY * dt);
+		computeDistanceLeft();
+		double step;
 		
-		// TODO: kiedy komórki s¹ zbyt blisko jest klops...
+		if (!started) {
+			step = from.radius + moveOverlap; // do not start exactly at source cell's center
+			started = true;
+		} else
+			step = MOVE_VELOCITY * dt;
 		
-		if (finished)
+		if (step > distanceLeft - (to.radius + moveOverlap)) {
+			finished = true;
 			to.getShotAtBy(this);
+		}
+		else
+			moveCloserBy(step);
 	}
 }
